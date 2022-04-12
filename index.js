327ac634f8485b9d03da38b570954ea2f132f926
jemel <jemel.defreitas@gmail.com>
2022-04-11 22:16:10 -0500

    commit tree f8c8ff1fcbcd19a1a93da2c2179e26c8574c5007

---
+++ index.js
@@ -0,0 +1,48 @@
+let num1 = 100
+let num2 = 2000
+const divide = function() {
+    return num2/ num1;
+}
+divide();
+
+const square = (x) => x*x;
+//square(x);
+
+const add = (a,b) => a+b
+
+//function add() {a,b => a+b}
+
+    
+
+
+
+
+
+
+
+//const divide = (num1, num2) => {
+  //  console.log(`${num1 = 2000}`);
+    //console.log(`${num2 = 100}`);
+    //return num1 / num2;
+  //}
+//sum(num1,num2);
+  
+  
+
+//const square = x => x * x;
+//square (x);
+
+
+
+
+
+  
+
+
+//const add = (a, b) => {
+//console.log(`${a = 3}`);
+//console.log(`${b= 4}`);
+  //  return a + b;
+  //}
+  //sum(3,4);
+ 
